31136dadbd62da344d1e58903350e98a6709135c
Marvin <marvin.vogl@gmail.com>
2022-05-11 11:56:48 +0200

    refactored choice image

--- lib/src/widgets/choice_image.dart
+++ lib/src/widgets/choice_image.dart
@@ -15,9 +15,13 @@ class ChoiceImage extends StatelessWidget {
     if (hand != null) {
       return Container(
         decoration: BoxDecoration(
-          color: Colors.white,
-          image: DecorationImage(image: AssetImage('images/${hand!.name}.png')),
-          border: Border.all(color: const Color(0xff601155), width: 2.0),
+          image: DecorationImage(
+            image: AssetImage('images/${hand!.name}.png'),
+          ),
+          border: Border.all(
+            color: const Color(0xff601155),
+            width: 2.0,
+          ),
           shape: BoxShape.circle,
         ),
         margin: const EdgeInsets.all(4.0),
